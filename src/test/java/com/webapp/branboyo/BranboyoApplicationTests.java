6102ce880760fc6e3c5bf59920f2fb5c95febad8
branboyo <branboyo@brandons-air.fios-router.home>
2020-08-12 18:03:58 -0400

    Has homepage and hyperlinks to Git, LinkedIn, and email

---
+++ src/test/java/com/webapp/branboyo/BranboyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.branboyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranboyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
